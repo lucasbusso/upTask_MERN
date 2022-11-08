445198c37b19ed9c80779a7ffdfa78cc28bd6474
Lucas Busso <lucasxbusso@gmail.com>
2022-11-07 22:41:23 -0300

    token auth and user password validation

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -3,50 +3,50 @@ import generarId from "../helpers/generarId.js";
 import generarJWT from "../helpers/generarJWT.js";
 
 const registrar = async (req, res) => {
-    //Evitar registro duplicado
+  const { email } = req.body; //para extraer valores de un formulario es con req.body
+  const existeUsuario = await Usuario.findOne({ email });
+  
+  
+  //Evitar registro duplicado
+  if (existeUsuario) {
+    const error = new Error("Este email ya ha sido utilizado");
+    return res.status(400).json({ msg: error.message });
+  }
 
-    const {email} = req.body;
-    const existeUsuario = await Usuario.findOne({email});
-
-    if(existeUsuario) {
-        const error = new Error('Este email ya ha sido utilizado');
-        return res.status(400).json({msg: error.message})
-    }
-    try {
-        const usuario = new Usuario(req.body);
-        usuario.token = generarId();
-        const usuarioAlmacenado = await usuario.save(); 
-        res.json(usuarioAlmacenado);
-
-    } catch (error) {
-        console.log(error)
-    }
-
+  try {
+    const usuario = new Usuario(req.body);
+    usuario.token = generarId();
+    const usuarioAlmacenado = await usuario.save();
+    res.json(usuarioAlmacenado);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 const autenticar = async (req, res) => {
-    const {email, password} = req.body;
-    const usuario = await Usuario.findOne({email});
+  const { email, password } = req.body; //para extraer valores de un formulario es con req.body
+  const usuario = await Usuario.findOne({ email });
 
   //Comprobar si el usuario existe
-    if(!usuario) {
-        const error = new Error("El usuario no existe");
-        return res.status(404).json({msg: error.message});
-    }
+  if (!usuario) {
+    const error = new Error("El usuario no existe");
+    return res.status(404).json({ msg: error.message });
+  }
 
   //Comprobar si el usuario esta confirmado
-   if (!usuario.confirmado) {
-     const error = new Error("El email no ha sido verificado");
-     return res.status(403).json({ msg: error.message });
-   }
+  if (!usuario.confirmado) {
+    const error = new Error("El email no ha sido verificado");
+    return res.status(403).json({ msg: error.message });
+  }
+
   //Comprobar el password
-  if(await usuario.comprobarPassword(password)){
+  if (await usuario.comprobarPassword(password)) {
     res.json({
-        _id: usuario._id,
-        nombre: usuario.nombre,
-        email: usuario.email,
-        token: generarJWT(usuario._id)
-    })
+      _id: usuario._id,
+      nombre: usuario.nombre,
+      email: usuario.email,
+      token: generarJWT(usuario._id),
+    });
   } else {
     const error = new Error("Contraseña incorrecta");
     return res.status(403).json({ msg: error.message });
@@ -54,8 +54,9 @@ const autenticar = async (req, res) => {
 }
 
 const confirmar = async (req, res) => {
-    const {token} = req.params;
+    const {token} = req.params;  //para extraer valores de la url es con req.params
     const usuarioConfirmar = await Usuario.findOne({token});
+
     if(!usuarioConfirmar) {
         const error = new Error("Token no valido");
         return res.status(403).json({ msg: error.message });
@@ -67,8 +68,76 @@ const confirmar = async (req, res) => {
         await usuarioConfirmar.save();
         res.json({msg: "Verificación exitosa"})
     } catch (error) {
-        
+        console.log(error);
     }
 }
 
-export { registrar, autenticar, confirmar };
+const recuperarContraseña = async (req, res) => {
+  const { email } = req.body; //para extraer valores de un formulario es con req.body
+  const usuario = await Usuario.findOne({ email });
+
+  if (!usuario) {
+    const error = new Error("El usuario no existe");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  try {
+    usuario.token = generarId();
+    await usuario.save();
+    res.json({ msg: "Revisa tu correo" });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const comprobarToken = async (req, res) => {
+  const { token } = req.params; //para extraer valores de la url es con req.params
+  const tokenValido = await Usuario.findOne({ token });
+
+  if(tokenValido) {
+    res.json({ msg: "Token válido, el usuario existe"});
+  } else {
+        const error = new Error("El token no es válido");
+        return res.status(404).json({ msg: error.message });
+  }
+}
+
+const nuevoPassword = async (req, res) => {
+  const { token } = req.params;
+  const { password } = req.body;
+  const usuario = await Usuario.findOne({ token });
+
+
+  if (usuario) {
+    usuario.password = password;
+    usuario.token = '';
+
+    try {
+      await usuario.save()
+      return res.json({msg: "Contraseña modificada correctamente"})
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    const error = new Error("El token no es válido");
+    return res.status(404).json({ msg: error.message });
+  }
+  console.log(token);
+  console.log(password);
+};
+
+const perfil = async (req, res) => {
+  const { usuario } = req;
+
+  res.json(usuario);
+}
+
+export {
+  registrar,
+  autenticar,
+  confirmar,
+  recuperarContraseña,
+  comprobarToken,
+  nuevoPassword,
+  perfil,
+};
--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -1,14 +1,25 @@
 import express from "express";
-const router = express.Router();
 import {
   registrar,
   autenticar,
   confirmar,
+  recuperarContraseña,
+  comprobarToken,
+  nuevoPassword,
+  perfil,
 } from "../controllers/usuarioController.js";
+import checkAuth from "../middleware/checklAuth.js";
+
+const router = express.Router();
 
 //Autenticacion, confirmar y confirmacion de usuarios
 router.post("/", registrar);
 router.post("/login", autenticar);
 router.get("/confirmar/:token", confirmar);
+router.post("/recuperar-password", recuperarContraseña);
+router.route("/recuperar-password/:token")
+  .get(comprobarToken)
+  .post(nuevoPassword);
+router.get("/perfil", checkAuth, perfil);
 
 export default router;
