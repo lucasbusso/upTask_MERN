7b41dee5064bc41158f74cc4a5e14d4b9ccb54f3
Lucas Busso <lbusso@minutentag.com>
2022-11-10 18:19:06 -0300

    Delete task method

--- backend/controllers/tareaController.js
+++ backend/controllers/tareaController.js
@@ -64,7 +64,26 @@ const actualizarTarea = async (req, res) => {
         console.log(error);
     }
 };
-const eliminarTarea = async (req, res) => {};
+const eliminarTarea = async (req, res) => {
+    const { id } = req.params;
+    const tarea = await Tarea.findById(id).populate("proyecto");
+
+    if(!tarea) {
+        const error = new Error("No se ha encontrado esta tarea");
+        return res.status(404).json({ msg: error.message });
+    }
+    if(tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+        const error = new Error("No tienes permisos para acceder a esta tarea");
+        return res.status(403).json({ msg: error.message });
+    }
+
+    try {
+        await tarea.deleteOne();
+        res.json({ msg: "Tarea eliminada"})
+    } catch (error) {
+        console.log(error)
+    }
+};
 const cambiarEstado = async (req, res) => {};
 
 export {
