cc5381442f2816e73cd95159c1ba29a0a8019c33
Lucas Busso <lbusso@minutentag.com>
2022-11-12 20:41:48 -0300

    Sending confirmation email with nodemailer and libk button to Confirmation Page

--- backend/helpers/email.js
+++ backend/helpers/email.js
@@ -1,5 +1,31 @@
 import nodemailer from "nodemailer";
 
-export const registerEmail = (datos) => {
-    console.log("datos", datos)
+export const registerEmail = async (datos) => {
+    const { email, name, token } = datos;
+
+    const transport = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: "9220b8459c72e2",
+          pass: "ed23264e2aa141"
+        }
+    });
+
+    //Informacion del email
+    const info = await transport.sendMail({
+        from:'"upTask - Manage your projects" <cuentas@upTask.com>',
+        to: email,
+        subject: 'upTask - Confirm your account',
+        text: 'Confirm your account in upTask',
+        html: `
+        <p>Hola ${name}. Verify your email to create your upTask account</p>
+        <p>Follow this link:</p>
+        <a href="${process.env.FRONTEND_URL}/confirm-account/${token}" 
+            style="text-decoration: none; padding: 6px; background: #0369a1; color: #fff; font-weight: 500; border-radius: 4px;">
+            Verificar email
+        </a>
+        <p>If you don't created this account, ignore this mail</p>
+        `,
+    })
 }
