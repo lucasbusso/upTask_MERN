631ce8a9daa0618da96c51ab3290fdb76c40e0f3
Lucas Busso <lbusso@minutentag.com>
2022-12-05 09:46:37 -0300

    UI Collaborator Tag to differenciate the projects you own

--- frontend/src/components/PreviewProject.jsx
+++ frontend/src/components/PreviewProject.jsx
@@ -1,17 +1,24 @@
 import { Link } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 const PreviewProject = ({proyecto}) => {
-  const { name, _id, client } = proyecto;
+  const { auth } = useAuth();
+  const { name, _id, client, creador } = proyecto;
     
     return (
-      <div className="border-b p-5 flex">
-        <p className="flex-1 font-bold">
-            {name} 
-            <small className="font-semibold"> for</small>
-            <span className="text-sm text-gray-500 uppercase font-semibold ">
-                {' '} {client}
-            </span>
-        </p>
+      <div className="border-b p-5 flex justify-between">
+        <div className="flex items-center gap-2">
+          <p className="flex-1 font-bold">
+              {name} 
+              <small className="font-semibold"> for</small>
+              <span className="text-sm text-gray-500 uppercase font-semibold ">
+                  {' '} {client}
+              </span>
+          </p>
+          {auth._id !== creador && 
+            <p className="p-1 text-xs rounded bg-green-600 text-white font-semibold">Collaborator</p>
+          }
+        </div>
         <Link 
             to={`${_id}`}
             className="text-gray-600 hover:text-gray-700 uppercase text-sm font-bold"    
